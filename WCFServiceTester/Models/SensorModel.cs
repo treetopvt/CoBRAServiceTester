d2471dd5f85aed6f4e89f4388873f7d65280acf7
BradGardner <captain_bg@hotmail.com>
2014-01-15 15:08:51 -0500

    update

---
+++ WCFServiceTester/Models/SensorModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFServiceTester.Models
+{
+    public class SensorModel
+    {
+        public Guid SensorGUID { get; set; }
+        public string SensorName { get; set; }
+        public string Description { get; set; }
+        public string SensorType { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateLastModified { get; set; }
+
+    }
+}
